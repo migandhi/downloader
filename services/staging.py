22a4975a0b540e754701bd050c6b4d545c9f59d2
migandhi <murtaza.i.gandhi@gmail.com>
2023-12-17 22:02:29 +0530

    Update staging.py

--- services/staging.py
+++ services/staging.py
@@ -5,13 +5,13 @@ class Staging:
 
     def __init__(self,prefix):
         self.uniquename = uuid.uuid1()
-        self.staging_path = '/data/{}/{}'.format(prefix.replace(' ','_') , self.uniquename  )
+        self.staging_path = 'data/{}/{}'.format(prefix.replace(' ','_') , self.uniquename  )
 
     def run(self):
         self.free()
         cmd = "mkdir -p {staging_path}".format(staging_path=self.staging_path)
         #output = os.popen(cmd).read()
-        os.makedirs(self.staging_path)
+        os.mkdir(self.staging_path)
         return self.staging_path
 
     def free(self):
